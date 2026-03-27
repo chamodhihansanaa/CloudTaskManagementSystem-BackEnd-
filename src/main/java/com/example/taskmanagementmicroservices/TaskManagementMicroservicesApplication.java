e9c21e62f7c2b54182941bf731f70971414865fb
chamodhi <ipc20hansana@gmail.com>
2026-03-27 10:56:54 +0530

    Initial commit - Backend project

---
+++ src/main/java/com/example/taskmanagementmicroservices/TaskManagementMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskmanagementmicroservices/TaskManagementMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
